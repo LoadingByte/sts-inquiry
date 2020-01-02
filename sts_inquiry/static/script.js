e2ad35d2b2fb39ec22f648e7d632ba1b2cfb7fd6
LoadingByte <hello@loadingbyte.com>
2020-01-02 18:07:40 +0100

    Fix tooltips not showing screenshots and maps when cluster size = 1

--- sts_inquiry/static/script.js
+++ sts_inquiry/static/script.js
@@ -1,29 +1,27 @@
 window.addEventListener("load", function () {
-    for (const cont of document.getElementsByClassName("stw-container")) {
-        for (const stw of cont.getElementsByClassName("stw")) {
-            const noscript = stw.getElementsByTagName("noscript")[0];
-            const mapPlaceholder = stw.getElementsByClassName("stw-tooltip-map-placeholder")[0];
+    for (const stw of document.querySelectorAll("td:nth-child(2) .stw")) {
+        const noscript = stw.getElementsByTagName("noscript")[0];
+        const mapPlaceholder = stw.getElementsByClassName("stw-tooltip-map-placeholder")[0];
 
-            if (noscript || mapPlaceholder) {
-                stw.addEventListener("mouseenter", function listener() {
-                    stw.removeEventListener("mouseenter", listener);
+        if (noscript || mapPlaceholder) {
+            stw.addEventListener("mouseenter", function listener() {
+                stw.removeEventListener("mouseenter", listener);
 
-                    if (noscript) {
-                        const content = noscript.textContent.trim();
-                        const wrapper = document.createElement("div");
-                        wrapper.innerHTML = content;
-                        noscript.parentNode.replaceChild(wrapper, noscript);
-                    }
+                if (noscript) {
+                    const content = noscript.textContent.trim();
+                    const wrapper = document.createElement("div");
+                    wrapper.innerHTML = content;
+                    noscript.parentNode.replaceChild(wrapper, noscript);
+                }
 
-                    if (mapPlaceholder) {
-                        const url = mapPlaceholder.textContent;
-                        const iframe = document.createElement("iframe");
-                        iframe.setAttribute("class", "stw-tooltip-map");
-                        iframe.setAttribute("src", url);
-                        mapPlaceholder.parentNode.replaceChild(iframe, mapPlaceholder);
-                    }
-                });
-            }
+                if (mapPlaceholder) {
+                    const url = mapPlaceholder.textContent;
+                    const iframe = document.createElement("iframe");
+                    iframe.setAttribute("class", "stw-tooltip-map");
+                    iframe.setAttribute("src", url);
+                    mapPlaceholder.parentNode.replaceChild(iframe, mapPlaceholder);
+                }
+            });
         }
     }
 });
